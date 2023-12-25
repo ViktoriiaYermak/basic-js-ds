ac12af1da6549a028cfcc5731d1a0a58c4f1e8be
ViktoriiaYermak <yermakvictoriia@gmail.com>
2023-12-25 15:24:36 +0200

    feat: completed task - binary-search-tree, (Mon, Dec 25, 2023 03:24:35 PM)

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -45,29 +45,80 @@ class BinarySearchTree {
     }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return this.searchNode(this.rootNode, data) !== null;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  searchNode(node, data) {
+    if (node === null) {
+      return null;
+    }
+
+    if (data < node.data) {
+      return this.searchNode(node.left, data);
+    } else if (data > node.data) {
+      return this.searchNode(node.right, data);
+    } else {
+      return node;
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return this.searchNode(this.rootNode, data);
+  }
+
+  remove(data) {
+    this.rootNode = this.removeNode(this.rootNode, data);
+  }
+
+  removeNode(node, data) {
+    if (node === null) {
+      return null;
+    }
+
+    if (data < node.data) {
+      node.left = this.removeNode(node.left, data);
+    } else if (data > node.data) {
+      node.right = this.removeNode(node.right, data);
+    } else {
+      if (node.left === null && node.right === null) {
+        return null;
+      } else if (node.left === null) {
+        return node.right;
+      } else if (node.right === null) {
+        return node.left;
+      } else {
+        const minRight = this.findMinNode(node.right);
+        node.data = minRight.data;
+        node.right = this.removeNode(node.right, minRight.data);
+      }
+    }
+
+    return node;
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const minNode = this.findMinNode(this.rootNode);
+    return minNode ? minNode.data : null;
+  }
+
+  findMinNode(node) {
+    while (node && node.left !== null) {
+      node = node.left;
+    }
+    return node;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const maxNode = this.findMaxNode(this.rootNode);
+    return maxNode ? maxNode.data : null;
+  }
+
+  findMaxNode(node) {
+    while (node && node.right !== null) {
+      node = node.right;
+    }
+    return node;
   }
 }
 
